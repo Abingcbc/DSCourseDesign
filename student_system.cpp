e934b9f50930947c0e795ee01293ac3b190b6cbb
Abingcbc <34068205+Abingcbc@users.noreply.github.com>
2018-10-08 18:30:23 +0800

    加入头尾节点

--- student_system.cpp
+++ student_system.cpp
@@ -20,17 +20,17 @@ private:
     string type;
     Student *pre;
     Student *next;
-    
+
 public:
     Student(int _id, string _name, string _sex, int _age, string _type,
             Student *_pre, Student *_next):
-    id(_id),
-    name(_name),
-    sex(_sex),
-    age(_age),
-    type(_type),
-    pre(_pre),
-    next(_next) {}
+            id(_id),
+            name(_name),
+            sex(_sex),
+            age(_age),
+            type(_type),
+            pre(_pre),
+            next(_next) {}
     ~Student() = default;
 
     void setPre(Student *pre_stu) {
@@ -41,20 +41,20 @@ public:
         next = next_stu;
     }
 
-    Student* getPre() {
+    Student* const getPre() {
         return this->pre;
     }
 
-    Student* getNext() {
+    Student* const getNext() {
         return this->next;
     }
 
-    void print() {
+    void const print() {
         cout << id << "      " << name << "    " << sex << "     "
-        << age << "     " << type << endl;
+             << age << "     " << type << endl;
     }
 
-    int getId() {
+    int const getId() {
         return this->id;
     }
 
@@ -70,12 +70,17 @@ public:
 class LinkedList {
 
 private:
-    Student *start_stu = nullptr;
-    Student *last_stu = nullptr;
+    Student *start_stu;
+    Student *last_stu;
     int all_num = 0;
 
 public:
-    LinkedList() = default;
+    LinkedList() {
+        start_stu = new Student(0, "first", "first", 0, "first", nullptr, nullptr);
+        last_stu = new Student(0, "last", "last", 0, "last", nullptr, nullptr);
+        start_stu->setNext(last_stu);
+        last_stu->setPre(start_stu);
+    }
 
     ~LinkedList() {
         while (start_stu != nullptr) {
@@ -83,119 +88,93 @@ public:
             start_stu = temp->getNext();
             delete temp;
         }
+        delete this;
     }
 
     void print() {
-        Student *temp = start_stu;
+        Student *temp = start_stu->getNext();
         cout << endl;
         cout << "考号    " << "姓名    " << "性别    " << "年龄    " << "报考类别" << endl;
-        while (temp != nullptr) {
+        while (temp != last_stu) {
             temp->print();
             temp = temp->getNext();
         }
     }
 
     void build(int _id, string _name, string _sex, int _age, string _type){
-        if (start_stu == nullptr) {
-            Student *temp_stu = new Student(_id, _name, _sex, _age, _type,
-                    nullptr, nullptr);
-            start_stu = temp_stu;
-            last_stu = temp_stu;
-        }
-        else {
-            Student *temp_stu = new Student(_id, _name, _sex, _age, _type,
-                    last_stu, nullptr);
-            last_stu->setNext(temp_stu);
-            last_stu = temp_stu;
-        }
+        Student *temp_stu = new Student(_id, _name, _sex, _age, _type,
+                                            last_stu->getPre(), last_stu);
+        last_stu->getPre()->setNext(temp_stu);
+        last_stu->setPre(temp_stu);
         all_num++;
     }
 
     void insert(int pos, int _id, string _name, string _sex, int _age, string _type) {
         Student *temp = start_stu;
         Student *stu_insert = new Student(_id, _name, _sex, _age, _type,
-                nullptr, nullptr);
-        if (pos == 1) {//When insert the first student, there is no previous one.
-            temp->setPre(stu_insert);
-            stu_insert->setNext(start_stu);
-            all_num++;
-        }
-        else if (pos > 1) {
-            for (int i = 0; i < (pos - 2) && temp != nullptr; i++) {
+                                          nullptr, nullptr);
+        if (pos >= 1) {
+            for (int i = 0; i < (pos - 1) && temp != last_stu; i++) {
                 temp = temp->getNext();
             }
             stu_insert->setNext(temp->getNext());
             stu_insert->setPre(temp);
-            if (temp->getNext() != nullptr) {
-                temp->getNext()->setPre(stu_insert);
-                temp->setNext(stu_insert);
-            }
-            else {
-                temp->setNext(stu_insert);
-            }
+            temp->getNext()->setPre(stu_insert);
+            temp->setNext(stu_insert);
             all_num++;
         }
         else {
-            cout << "请输入一个自然数！" << endl;
+            cout << "请输入一个正整数！" << endl;
         }
-
     }
 
     void remove(int id) {
         Student *temp = start_stu;
-        while (temp != nullptr) {
+        while (temp != last_stu) {
             if (temp->getId() == id)
                 break;
             temp = temp->getNext();
         }
-        if (temp == nullptr) {
+        if (temp == last_stu) {
             cout << "无此考号的考生" << endl;
         }
         else {
             cout << "你删除的考生信息是：";
             temp->print();
-            if (temp->getPre() != nullptr)
-                temp->getPre()->setNext(temp->getNext());
-            else {
-                start_stu = temp->getNext();
-            }
-            if (temp->getNext() != nullptr)
-                temp->getNext()->setPre(temp->getPre());
+            temp->getPre()->setNext(temp->getNext());
+            temp->getNext()->setPre(temp->getPre());
             delete temp;
             this->print();
             all_num--;
         }
     }
 
-    void find(int id) {
+    bool find(int id) {
         Student *temp = start_stu;
-        while (temp != nullptr) {
+        while (temp != last_stu) {
             if (temp->getId() == id) {
                 cout << "考号    " << "姓名    " << "性别    " << "年龄    " << "报考类别" << endl;
                 temp->print();
+                return true;
+            }
+            temp = temp->getNext();
+        }
+        if (temp == last_stu) {
+            cout << "无此考号的考生" << endl;
+            return false;
+        }
+    }
+
+    void reset(int id1, int _id, string _name, string _sex, int _age, string _type) {
+        Student *temp = start_stu;
+        while  (temp != last_stu) {
+            if (temp->getId() == id1) {
+                temp->reset(_id, _name, _sex, _age, _type);
                 break;
             }
             temp = temp->getNext();
         }
-        if (temp == nullptr) {
-            cout << "无此考号的考生" << endl;
-        }
-    }
-
-    void reset(int _id, string _name, string _sex, int _age, string _type) {
-        Student *temp = start_stu;
-        while  (temp != nullptr) {
-            if (temp->getId() == _id) {
-                temp->reset(_id, _name, _sex, _age, _type);
-                break;
-            }
-        }
-        if (temp == nullptr) {
-            cout << "无此考号的考生" << endl;
-        }
-        else {
-            this->print();
-        }
+        this->print();
     }
 
     int getNum() {
@@ -241,8 +220,8 @@ int main() {
                 int pos;
                 cout << "请输入你要插入的考生的位置：";
                 cin >> pos;
-                if (pos > (stu_list->getNum() + 1)) {
-                    cout << "请输入一个小于" << stu_list->getNum() + 1<< "的正整数！" << endl;
+                if (pos > (stu_list->getNum() + 1) || pos <= 0) {
+                    cout << "请输入一个小于等于" << stu_list->getNum() + 1<< "的正整数！" << endl;
                     break;
                 }
                 cout << "请依次输入要插入的考生的考号，姓名，性别，年龄及报考类别！" << endl;
@@ -260,4 +239,24 @@ int main() {
                 cin >> id;
                 stu_list->find(id);
                 break;
-            case 4
+            case 4:
+                int id1;
+                cout << "请输入要修改的考生的考号：";
+                cin >> id1;
+                if (stu_list->find(id1)) {
+                    cout << "请依次输入要修改的考生的考号，姓名，性别，年龄及报考类别！" << endl;
+                    cin >> id >> name >> sex >> age >> type;
+                    stu_list->reset(id1, id, name, sex, age, type);
+                }
+                break;
+            case 5:
+                stu_list->print();
+                break;
+            default:
+                cout << "输入的操作数不存在，请重新输入！" << endl;
+                break;
+        }
+        cout << "请输入您要进行的操作：";
+    }
+    return 0;
+}
